ae9fb749db2944bd1013b856f5c8ef2c672c5587
Your Name <czipa7@gmail.com>
2025-10-15 08:27:08 +0200

    fix: resume linkedin link

--- src/Sections/Resume.tsx
+++ src/Sections/Resume.tsx
@@ -3,10 +3,10 @@ import { FC } from "react";
 const Resume: FC = () => {
   const handleDownload = () => {
     // Replace with your actual resume file path
-    const resumeUrl = "/Andras-Czipa-Frontend-Resume.pdf";
+    const resumeUrl = "/andras-czipa-frontend-resume.pdf";
     const link = document.createElement("a");
     link.href = resumeUrl;
-    link.download = "Andras-Czipa-Frontend-Resume.pdf";
+    link.download = "andras-czipa-frontend-resume.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
